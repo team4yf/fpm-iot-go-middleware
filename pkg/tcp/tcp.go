d243553f473277174415a76332510b06357ecdde
yfsoftcom <yfsoftcom@126.com>
2020-08-08 09:11:39 +0800

    todo reflect the tcp

--- pkg/tcp/tcp.go
+++ pkg/tcp/tcp.go
@@ -8,27 +8,31 @@ package tcp
 import (
 	"fmt"
 	"net"
+	"github.com/team4yf/fpm-iot-go-middleware/pkg/utils"
 )
 
 //ReceiveHandler the receive handler
-//topic is the #sn/tcp/receive
+//clientID is the random id of the tcp connection
 //data  is the origin payload
-type ReceiveHandler func(topic string, data []byte)
+type ReceiveHandler func(clientID string, data []byte)
 
 //NetReceiver the interface for the tcp receiver
 //Read is the only one method should be implemented! it's just run the tcp server on a port
 type NetReceiver interface {
-	Read(port int)
+	Read(max int)
+	Write(clientID string, buf []byte) error
+	Listen(port int)
 }
 
 type netReceiver struct {
 	handler ReceiveHandler
+	clients map[string]net.Conn
 }
 
 //Read
 //start to open a socket server on the port
 //use go routine to read, call the handler after received the message data.
-func (r *netReceiver) Read(port int) {
+func (r *netReceiver) Listen(port int) {
 	go func() {
 		// listen on all interfaces
 		ln, _ := net.Listen("tcp", fmt.Sprintf(":%d", port))
@@ -37,31 +41,43 @@ func (r *netReceiver) Read(port int) {
 		for {
 			// accept connection on port
 			conn, _ := ln.Accept()
-			//logs an incoming message
-			// Handle connections in a new goroutine.
-			go func() {
-				for {
-					buf := make([]byte, 1024)
-					reqLen, err := conn.Read(buf)
-					if err != nil {
-						fmt.Println("Error to read message because of ", err)
-						return
-					}
-					protocol := buf[0]
-					if protocol != 0xee {
-						// only support protocol of startsWith ee
-						return
-					}
-					data := buf[15:reqLen]
-					// output message received
-					r.handler("#socket/ee", data)
-				}
-			}()
-
+			clientID := utils.GenShortID()
+			r.clients[clientID] = conn
 		}
 	}()
 
 }
+
+func (r *netReceiver) Read(max int) {
+		
+	// run loop forever (or until ctrl-c)
+	for clientID, conn := range r.clients {
+		//logs an incoming message
+		// Handle connections in a new goroutine.
+		go func() {
+			for {
+				buf := make([]byte, max)
+				reqLen, err := conn.Read(buf)
+				if err != nil {
+					fmt.Println("Error to read message because of ", err)
+					return
+				}
+				
+				data := buf[15:reqLen]
+				// output message received
+				r.handler(clientID, data)
+			}
+		}()
+
+	}
+
+}
+
+
+func (r *netReceiver) Write(clientID string, buf []byte) error {
+	return nil
+}
+
 
 //NewNetReceiver create a new receiver
 func NewNetReceiver(f ReceiveHandler) NetReceiver {
